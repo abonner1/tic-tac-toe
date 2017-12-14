64ddbf2af241b09ea139370fd20a5b90aa4d7d1b
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-14 17:55:51 -0500

    Fix syntax in test.

--- spec/01_board_spec.rb
+++ spec/01_board_spec.rb
@@ -2,7 +2,7 @@ require "board"
 
 describe Board do
 
-  describe ".cells" do
+  describe "#cells" do
     it "returns an empty array" do
       expect(Board.new.cells).to match_array([" ", " ", " ", " ", " ", " ", " ", " ", " "])
     end

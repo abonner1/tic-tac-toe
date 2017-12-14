322615d96514e8d584db3e29b55f20eb6aee10e4
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-14 17:48:07 -0500

    Add .cells attribute test.

---
+++ spec/01_board_spec.rb
@@ -0,0 +1,10 @@
+require "board"
+
+describe Board do
+
+  describe ".cells" do
+    it "returns an empty array" do
+      expect(Board.new.cells).to match_array([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    end
+  end
+end
